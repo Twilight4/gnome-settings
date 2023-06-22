d3b053920ec0de779fb81eaa8104cfc9bd7e8e5c
Twilight4 <electrolight071@gmail.com>
2023-06-22 20:51:16 +0200

    update

--- gnome-scripts/.config/zsh/bash-scripts/switch-minimized.sh
+++ gnome-scripts/.config/zsh/bash-scripts/switch-minimized.sh
@@ -3,7 +3,9 @@
 # Since I switch windows using the same keybinds as in hyprland utilizing "focus changer" GNOME extension, I rarely use alt-tab
 # keybind so using this keybind instead as a method of cycling through only minimized windows which is equivalent of how I used
 # special workspaces in hyprland I'd find helpful (There's no extension for this functionality).
+# This script is bound to special keybind in gnome keyboard settings.
 
 xdotool search --class "^Navigator$|^Brave$|^cachybrowser$|^kitty$|^Lutris$|^Discord$" windowactivate %@ keydown Alt+Tab
 
-# This script is bound to special keybind in gnome keyboard settings.
+# To check the currently focus class name use command in terminal: sleep 1 && xdotool getactivewindow getwindowclassname
+# and then change focus desired window
