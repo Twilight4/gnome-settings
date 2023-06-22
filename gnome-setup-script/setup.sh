fdacbe18cc85be3ba44eae5b59abd9d5c2ff3515
Twilight4 <electrolight071@gmail.com>
2023-06-23 01:41:34 +0200

    update

--- gnome-setup-script/setup.sh
+++ gnome-setup-script/setup.sh
@@ -5,11 +5,12 @@
 # Disable minimise, maximise and close buttons from all windows
 gsettings set org.gnome.desktop.wm.preferences button-layout :
 
-echo Post-Installation:'
+# Swap esc with caps key
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
+
+echo '
 - Check if DBus service is launched (for kde connect)
     gapplication launch org.gnome.Shell.Extensions.GSConnect
     - if it returns error message then restart the DBus session
     systemctl --user reload dbus-broker.service
-    - If you are on GNOME then to switch caps with esc issue command (it should be in dconf database)
-    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
 '
