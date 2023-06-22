0f467d6966ea8cea69ec4172f08bf1a7700b997e
Twilight4 <electrolight071@gmail.com>
2023-06-22 20:38:00 +0200

    update

--- gnome-scripts/switch-minimized.sh
+++ gnome-scripts/switch-minimized.sh
@@ -4,6 +4,6 @@
 # keybind so using this keybind instead as a method of cycling through only minimized windows which is equivalent of how I used
 # special workspaces in hyprland I'd find helpful (There's no extension for this functionality).
 
-xdotool search --class "^Navigator$|^Brave$|^cachybrowser$|^kitty$|^Lutris$" windowactivate %@ keydown Alt+Tab
+xdotool search --class "^Navigator$|^Brave$|^cachybrowser$|^kitty$|^Lutris$|^Discord$" windowactivate %@ keydown Alt+Tab
 
 # This script is bound to special keybind in gnome keyboard settings.
