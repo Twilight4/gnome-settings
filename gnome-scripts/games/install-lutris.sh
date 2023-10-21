b0c041fe0d34ff07a8eef8a84da1e532c27d61ce
Twilight4 <electrolight071@gmail.com>
2023-10-21 19:52:50 +0200

    update

--- gnome-scripts/games/install-lutris.sh
+++ gnome-scripts/games/install-lutris.sh
@@ -11,3 +11,6 @@ lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjp
 sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
 ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+
+# Try using CachyOS's package for esential gaming packages
+sudo pacman -S cachyos-gaming-meta
